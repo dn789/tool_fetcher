b6de81fa398632c1ac172c0d1993dcd978713830
Danny <nassre@gmail.com>
2023-03-19 15:56:11 -0400

    moved shared functions from make_corpus to utils

--- backend/find_terms/make_corpus.py
+++ backend/find_terms/make_corpus.py
@@ -11,101 +11,14 @@ from tqdm.auto import tqdm
 from bs4 import BeautifulSoup
 
 from .utils import (
-    get_alpha_prop,
-    get_terms_from_tagged_sent,
+    get_terms_from_tagged_sents,
+    make_re_patterns,
+    match_terms_in_sents,
+    normalize_term,
     prepare_sent_NER,
     sent_filter,
     sent_tokenize_web_doc)
-from . import pdf_extract
-
-
-def make_re_patterns(terms_ignore_case_path, terms_keep_case_path=None):
-    """
-    Makes regex patterns to find terms in text.
-
-    Parameters
-    ----------
-    terms_ignore_case_path : str
-        Path to file containing terms to match regardless of case formatting
-        (terms that don't have common word aliases).
-    terms_keep_case_path : str, optional
-        Path to file containing terms to match with exact case formatting. (
-        terms that do have common word aliases).
-    """
-    terms_ignore_case = set(open(terms_ignore_case_path, encoding='utf-8').read(
-    ).strip().split('\n')) if terms_ignore_case_path else set()
-    terms_keep_case = set(open(terms_keep_case_path, encoding='utf-8').read(
-    ).strip().split('\n')) if terms_keep_case_path else set()
-
-    for set_ in (terms_ignore_case, terms_keep_case):
-        update = set()
-        for term in set_:
-            if '-' in term:
-                update.add(term.replace('-', ' '))
-            if ' ' in term:
-                update.add(term.replace(' ', '-'))
-        set_.update(update)
-
-    terms_ignore_case = sorted(terms_ignore_case, key=len, reverse=True)
-    ignore_case_pattern = re.compile(
-        rf'\b({"|".join([re.escape(term) for term in terms_ignore_case])})\b',
-        re.IGNORECASE)
-    if terms_keep_case:
-        terms_keep_case = sorted(
-            terms_keep_case, key=len, reverse=True) if terms_keep_case_path else None
-        keep_case_pattern = re.compile(
-            rf'\b({"|".join([re.escape(term) for term in terms_keep_case])})\b')
-    else:
-        keep_case_pattern = None
-    return ignore_case_pattern, keep_case_pattern
-
-
-def match_terms_in_doc(patterns, sents, dont_filter_sents=False, neg_sents_proportion=0):
-    """Finds termss in document using re matching.
-
-    If dont_filter_sents, sentences longer than 100 words or with a proportion of
-    letters less than .9 (not including spaces) will not be excluded from tagging.
-    """
-    ignore_case_pattern, keep_case_pattern = patterns
-
-    tagged_sents = []
-    sents_total, sents_with_entities = 0, 0
-    for sent in sents:
-        if not dont_filter_sents and not sent_filter(sent):
-            continue
-        sent = prepare_sent_NER(sent)
-        spans = set()
-        spans.update([match.span()
-                     for match in re.finditer(ignore_case_pattern, sent)])
-        if keep_case_pattern:
-            spans.update([match.span()
-                          for match in re.finditer(keep_case_pattern, sent)])
-        spans = sorted(spans, key=lambda x: (x[0], -x[1]))
-
-        if not spans and neg_sents_proportion != -1:
-            if not neg_sents_proportion or not sents_total or (1 - (sents_with_entities / sents_total)) >= neg_sents_proportion:
-                continue
-        if spans:
-            sents_with_entities += 1
-        sents_total += 1
-        last_stop = 0
-        tagged_sent = []
-        for index, (start, stop) in enumerate(spans):
-            if index != 0 and start in spans[index - 1]:
-                continue
-            tagged_sent.extend([(word, 'O')
-                               for word in sent[last_stop:start].split()])
-            tagged_sent.extend([(word, 'B-Term' if not index else 'I-Term')
-                               for index, word in enumerate(sent[start:stop].split())])
-            last_stop = stop
-        if last_stop != len(sent):
-            tagged_sent.extend([(word, 'O')
-                               for word in sent[last_stop:].split()])
-        if tagged_sent:
-            tagged_sent = '\n'.join(
-                [f'{word} {tag}' for word, tag in tagged_sent])
-            tagged_sents.append(tagged_sent)
-    return tagged_sents
+from . import pdf_utils
 
 
 def add_to_summary(summary, words):
@@ -118,17 +31,17 @@ def get_file_summary(corpus_summary, filename, sents):
     file_summary = corpus_summary['files'][filename] = {}
     for sent in sents:
         word_count = len(sent.split('\n'))
-        corpus_summary['total']['sent_count'] += 1
-        corpus_summary['total']['word_count'] += word_count
-        terms = get_terms_from_tagged_sent(sent)
-        add_to_summary(file_summary, terms)
+        corpus_summary['corpus']['total']['sent_count'] += 1
+        corpus_summary['corpus']['total']['word_count'] += word_count
+    terms = get_terms_from_tagged_sents(*sents)
+    add_to_summary(file_summary, terms)
 
 
 def get_out_of_vocabulary_prop(train_test_sets):
     """
     Reports type and token out-of-vocabulary proportion in test(dev and/or
     test) sets, i.e. the proportion of entity types and tokens in the test sets
-    that don't occur in the training set.
+    that don't occur in the training set. (Approximate)
     """
     train_sents = train_test_sets[0]
     test_sents = []
@@ -136,11 +49,11 @@ def get_out_of_vocabulary_prop(train_test_sets):
         test_sents.extend(set_)
     assert len(train_sents) > len(test_sents)
     train_dict, test_dict = {}, {}
-    for index, sent_set in enumerate((train_sents, test_sents)):
-        for sent in sent_set:
-            terms = get_terms_from_tagged_sent(sent)
-            dict_ = train_dict if not index else test_dict
-            add_to_summary(dict_, terms)
+    for sents in (train_sents, test_sents):
+        dict_ = train_dict if sents is train_sents else test_dict
+        terms = [normalize_term(term) for term in set(
+            get_terms_from_tagged_sents(*sents))]
+        add_to_summary(dict_, terms)
     type_oov = set(test_dict.keys()).difference(set(train_dict.keys()))
     type_oov_count = len(type_oov)
     type_oov_prop = type_oov_count / len(test_dict)
@@ -153,6 +66,91 @@ def get_out_of_vocabulary_prop(train_test_sets):
         'token_oov_prop': round(token_oov_prop, 2)
     }
     return overlap_dict
+
+
+def replace_terms_with_oov(term_patterns, term_sets, train_sents, test1_sents, test2_sents=()):
+    """
+    Replaces terms in test and dev sentences with terms that don't occur in 
+    train sentences.
+    """
+    ignore_case_pattern, keep_case_pattern = term_patterns
+    ignore_case_terms, _ = term_sets
+    ignore_case_terms = set([term.lower().replace('-', ' ')
+                            for term in ignore_case_terms])
+    vocab = set(get_terms_from_tagged_sents(*train_sents))
+    vocab_ignore_case, vocab_keep_case = set(), set()
+    for term in vocab:
+        if re.match(ignore_case_pattern, rf'{term}$'):
+            vocab_ignore_case.add(term)
+        if re.match(keep_case_pattern, rf'{term}$'):
+            vocab_keep_case.add(term)
+    vocab_ignore_case_pattern, vocab_keep_case_pattern = make_re_patterns(
+        vocab_ignore_case, vocab_keep_case)
+    all_oov_terms = []
+    for term in ignore_case_terms:
+        if not re.match(vocab_ignore_case_pattern, rf'{term}$'):
+            all_oov_terms.append(term)
+    return_sets = {}
+    for sents in (test1_sents, test2_sents):
+        key = 'test1' if sents is test1_sents else 'test2'
+        terms = set(get_terms_from_tagged_sents(*sents))
+        if test2_sents:
+            if key == 'test1':
+                oov_terms = all_oov_terms[:round(len(all_oov_terms) / 2)]
+            else:
+                oov_terms = all_oov_terms[round(len(all_oov_terms) / 2):]
+        else:
+            oov_terms = all_oov_terms
+
+        replace_dict = {}
+
+        def re_sub_oov(match):
+            return replace_dict.get(match.group(0), match.group(0))
+
+        oov_index = 0
+        last_oov_index = len(oov_terms) - 1
+        for term in terms:
+            if oov_index > last_oov_index:
+                oov_index = 0
+            replace_dict[term] = oov_terms[oov_index]
+            oov_index += 1
+
+        for index, sent in enumerate(sents):
+            if not get_terms_from_tagged_sents(sent):
+                continue
+            sent = ' '.join([line.split()[0] for line in sent.split('\n')])
+            sent = re.sub(vocab_ignore_case_pattern, re_sub_oov, sent)
+            if vocab_keep_case_pattern:
+                sent = re.sub(vocab_keep_case_pattern, re_sub_oov, sent)
+            if key == 'test2':
+                sent = re.sub(test1_terms_ignore_case_pattern,
+                              re_sub_oov, sent)
+                if test1_terms_keep_case_pattern:
+                    sent = re.sub(test1_terms_keep_case_pattern,
+                                  re_sub_oov, sent)
+            sent = match_terms_in_sents(
+                term_patterns, sent, dont_filter_sents=True)[0]
+            sents[index] = sent
+        return_sets[key] = sents
+
+        if key == 'test1' and test2_sents:
+            test_terms_ignore_case, test_terms_keep_case = set(), set()
+            for term in terms:
+                if re.match(vocab_ignore_case_pattern, f'{term}$') or re.match(vocab_keep_case_pattern, f'{term}$'):
+                    continue
+                if re.match(ignore_case_pattern, f'{term}$'):
+                    test_terms_ignore_case.add(term)
+                if re.match(keep_case_pattern, f'{term}$'):
+                    test_terms_keep_case.add(term)
+            test1_terms_ignore_case_pattern, test1_terms_keep_case_pattern = make_re_patterns(
+                test_terms_ignore_case, test_terms_keep_case)
+    train_terms = set(get_terms_from_tagged_sents(*train_sents))
+    test1_terms = set(get_terms_from_tagged_sents(*test1_sents))
+    test2_terms = set(get_terms_from_tagged_sents(*test2_sents))
+    assert not train_terms & test1_terms
+    assert not train_terms & test2_terms
+    assert not test1_terms & test2_terms
+    return return_sets
 
 
 def make_train_test_sets(folder, args_ref, train_test_split=(85, 15)):
@@ -177,8 +175,8 @@ def make_train_test_sets(folder, args_ref, train_test_split=(85, 15)):
 
     for terms_dict in corpus_summary['files'].values():
         for term, count in terms_dict.items():
-            corpus_summary['total']['terms'].setdefault(term, 0)
-            corpus_summary['total']['terms'][term] += count
+            corpus_summary['corpus']['total']['terms'].setdefault(term, 0)
+            corpus_summary['corpus']['total']['terms'][term] += count
 
     random.shuffle(sents)
     assert sum(train_test_split) == 100
@@ -189,10 +187,10 @@ def make_train_test_sets(folder, args_ref, train_test_split=(85, 15)):
         if index == len(train_test_split) - 1:
             sets.append(sents[last_index:])
         else:
-            sets.append(sents[last_index:count])
-            last_index = count
+            sets.append(sents[last_index:last_index + count])
+            last_index += count
     oov_dict = get_out_of_vocabulary_prop(sets)
-    corpus_summary['oov'] = oov_dict
+    corpus_summary['corpus']['oov'] = oov_dict
     return sets, corpus_summary
 
 
@@ -202,6 +200,7 @@ def make_corpus(input_folder,
                 dont_filter_sents=False,
                 neg_sents_proportion_pdf=0,
                 neg_sents_proportion_web=0,
+                replace_test_terms_with_oov=False,
                 ** kwargs):
     """Uses provided terms to make a training NER corpus.
 
@@ -250,6 +249,7 @@ def make_corpus(input_folder,
         'dont_filter_sents': dont_filter_sents,
         'neg_sents_proportion_pdf': neg_sents_proportion_pdf,
         'neg_sents_proportion_web': neg_sents_proportion_web,
+        'replace_test_terms_with_oov': replace_test_terms_with_oov,
         'kwargs': kwargs
     }
 
@@ -258,9 +258,14 @@ def make_corpus(input_folder,
         'terms_ignore_cast_path', 'data/find_terms/tool_names_ignore_case.txt')
     terms_keep_case_path = kwargs.get(
         'terms_keep_cast_path', 'data/find_terms/tool_names_keep_case.txt')
-    patterns = None
+    terms_ignore_case = set(open(
+        terms_ignore_case_path, encoding='utf-8').read().split('\n'))
+    terms_keep_case = set(open(terms_keep_case_path,
+                               encoding='utf-8').read().split('\n'))
+    term_patterns = make_re_patterns(
+        terms_ignore_case, terms_keep_case)
 
-    text_folder = os.path.join(output_folder, 'converted_text')
+    text_folder = os.path.join(output_folder, 'text')
     tokenized_folder = os.path.join(output_folder, 'sentence_tokenized')
     tagged_folder = os.path.join(output_folder, 'tagged')
     train_test_split_folder = os.path.join(output_folder, 'train_test_sets')
@@ -300,7 +305,7 @@ def make_corpus(input_folder,
                     f.write('\n'.join(sents))
                 continue
             elif ext == '.pdf':
-                text = pdf_extract.pdf_to_txt(
+                text = pdf_utils.pdf_to_txt(
                     os.path.join(input_folder, filename))
             elif ext in ('.htm', '.html'):
                 html = open(os.path.join(input_folder, filename),
@@ -316,13 +321,16 @@ def make_corpus(input_folder,
                          position=0,
                          leave=True):
         file_ref = os.path.splitext(filename)[0]
+        # Uses web doc tokenization for text files manually added to text folder.
+        if file_ref not in log:
+            log[file_ref] = {'ext': '.html'}
         ext = log[file_ref]['ext']
         if f'{file_ref}.txt' in (os.listdir(tokenized_folder) +
                                  os.listdir(tagged_folder)) or log[file_ref].get('no_terms'):
             continue
         sents = None
         if ext == '.pdf':
-            sents = pdf_extract.tokenize_doc(
+            sents = pdf_utils.tokenize_doc_from_filepath(
                 os.path.join(text_folder, filename), sent_tokenize,
                 use_line_ends=use_line_ends)
         elif ext in ('.htm', '.html'):
@@ -338,17 +346,14 @@ def make_corpus(input_folder,
                          leave=True):
         file_ref = os.path.splitext(filename)[0]
         ext = log[file_ref]['ext']
-        if f'{file_ref}.txt' in os.listdir(tagged_folder) or log[file_ref]['no_terms']:
+        if f'{file_ref}.txt' in os.listdir(tagged_folder) or log[file_ref].get('no_terms'):
             continue
-        if not patterns:
-            patterns = make_re_patterns(
-                terms_ignore_case_path, terms_keep_case_path)
         sents = open(os.path.join(tokenized_folder, filename),
                      encoding='utf-8').read().strip().split('\n')
         sents = set(sents)
         neg_sents_proportion = neg_sents_proportion_pdf if ext == '.pdf' else neg_sents_proportion_web
-        tagged_sents = match_terms_in_doc(
-            patterns, sents, dont_filter_sents=dont_filter_sents, neg_sents_proportion=neg_sents_proportion)
+        tagged_sents = match_terms_in_sents(
+            term_patterns, *sents, dont_filter_sents=dont_filter_sents, neg_sents_proportion=neg_sents_proportion)
         if tagged_sents:
             with open(os.path.join(tagged_folder, filename), 'w', encoding='utf-8') as f:
                 f.write('\n\n'.join(tagged_sents))
@@ -359,12 +364,19 @@ def make_corpus(input_folder,
     if os.listdir(tagged_folder):
         sets, summary = make_train_test_sets(
             tagged_folder, args_ref, train_test_split=train_test_split)
+
+        if replace_test_terms_with_oov:
+            return_sets = replace_terms_with_oov(
+                term_patterns, (terms_ignore_case, terms_keep_case), *sets)
+            sets = [set_ for set_ in (
+                sets[0], return_sets['test1'], return_sets['test2']) if set_]
+            summary['corpus'].pop('oov')
         labels = {0: 'train.txt', 1: 'dev.txt', 2: 'test.txt'}
         for index, set_ in enumerate(sets):
             with open(os.path.join(train_test_split_folder, labels[index]),
                       'w', encoding='utf-8') as f:
                 f.write('\n\n'.join(set_))
-        with open(os.path.join(train_test_split_folder, 'summary.json'),
+        with open(os.path.join(train_test_split_folder, 'corpus_summary.json'),
                   'w', encoding='utf-8') as f:
             f.write(json.dumps(summary))
 
--- backend/find_terms/utils.py
+++ backend/find_terms/utils.py
@@ -61,27 +61,116 @@ def get_alpha_prop(sent):
     return alnum_count / total
 
 
-def get_terms_from_tagged_sent(sent):
+def get_terms_from_tagged_sents(*sents):
     terms = []
-    current_term = []
-    lines = sent.split('\n')
-    last_index = len(lines) - 1
-    for index, line in enumerate(lines):
-        word, tag = line.split()
-        if tag == 'B-Term':
-            if current_term:
-                terms.append(' '.join(current_term))
-            current_term = [word]
-        elif tag == 'I-Term':
-            current_term.append(word)
-        elif tag == 'O':
-            if current_term:
-                terms.append(' '.join(current_term))
-                current_term = []
-        if index == last_index and current_term:
+    for sent in sents:
+        current_term = []
+        lines = sent.split('\n')
+        last_index = len(lines) - 1
+        for index, line in enumerate(lines):
+            word, tag = line.split()
+            if tag == 'B-Term':
+                if current_term:
+                    terms.append(' '.join(current_term))
+                current_term = [word]
+            elif tag == 'I-Term':
+                current_term.append(word)
+            elif tag == 'O':
+                if current_term:
+                    terms.append(' '.join(current_term))
+                    current_term = []
+            if index == last_index and current_term:
                 terms.append(' '.join(current_term))
 
     return terms
+
+
+def make_re_patterns(terms_ignore_case, terms_keep_case=set()):
+    """
+    Makes regex patterns to find terms in text.
+
+    Parameters
+    ----------
+    terms_ignore_case : iterable
+        Terms to match ignoring case (don't have common word aliases).
+    terms_keep_case : str, optional
+        Terms to match case-sensitively (do have common word aliases).
+    """
+    terms_ignore_case, terms_keep_case = set(
+        terms_ignore_case), set(terms_keep_case)
+    for set_ in (terms_ignore_case, terms_keep_case):
+        update = set()
+        for term in set_:
+            update.add(term.replace('-', ' '))
+            update.add(term.replace(' ', '-'))
+        set_.update(update)
+
+    terms_ignore_case = sorted(terms_ignore_case, key=len, reverse=True)
+    ignore_case_pattern = re.compile(
+        rf'\b({"|".join([re.escape(term) for term in terms_ignore_case])})\b',
+        re.IGNORECASE)
+    if terms_keep_case:
+        terms_keep_case = sorted(
+            terms_keep_case, key=len, reverse=True)
+        keep_case_pattern = re.compile(
+            rf'\b({"|".join([re.escape(term) for term in terms_keep_case])})\b')
+    else:
+        keep_case_pattern = None
+    return ignore_case_pattern, keep_case_pattern
+
+
+def match_terms_in_sents(term_patterns, *sents, dont_filter_sents=False, neg_sents_proportion=0):
+    """Finds termss in sents using re matching.
+
+    If dont_filter_sents, sentences shorter than 5 words or longer than 100 or 
+    with a proportion of letters less than .9 (not including spaces) will not 
+    be excluded from tagging.
+    """
+    ignore_case_pattern, keep_case_pattern = term_patterns
+    tagged_sents = []
+    sents_total, sents_with_entities = 0, 0
+
+    for sent in sents:
+        # print(sent)
+        if not dont_filter_sents and not sent_filter(sent):
+            continue
+        sent = prepare_sent_NER(sent)
+        spans = set()
+        spans.update([match.span()
+                     for match in re.finditer(ignore_case_pattern, sent)])
+        if keep_case_pattern:
+            spans.update([match.span()
+                          for match in re.finditer(keep_case_pattern, sent)])
+        spans = sorted(spans, key=lambda x: (x[0], -x[1]))
+
+        if not spans and neg_sents_proportion != -1:
+            if not neg_sents_proportion or not sents_total or (1 - (sents_with_entities / sents_total)) >= neg_sents_proportion:
+                continue
+        if spans:
+            sents_with_entities += 1
+        sents_total += 1
+        last_stop = 0
+        tagged_sent = []
+        for index, (start, stop) in enumerate(spans):
+            if index != 0 and start in spans[index - 1]:
+                continue
+            tagged_sent.extend([(word, 'O')
+                               for word in sent[last_stop:start].split()])
+            tagged_sent.extend([(word, 'B-Term' if not index else 'I-Term')
+                               for index, word in enumerate(sent[start:stop].split())])
+            last_stop = stop
+        if last_stop != len(sent):
+            tagged_sent.extend([(word, 'O')
+                               for word in sent[last_stop:].split()])
+        if tagged_sent:
+            tagged_sent = '\n'.join(
+                [f'{word} {tag}' for word, tag in tagged_sent])
+            tagged_sents.append(tagged_sent)
+    return tagged_sents
+
+
+def normalize_term(term):
+    return ''.join([char for char in term if char.isalnum()])
 
 
 def prepare_sent_NER(sent):
