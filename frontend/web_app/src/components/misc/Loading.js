a8940ce4c483407433826f227f5e4418b0803d90
dn789 <61817857+dn789@users.noreply.github.com>
2023-08-06 06:10:08 -0400

    Delete frontend/web_app directory

--- frontend/web_app/src/components/misc/Loading.js
+++
@@ -1,9 +0,0 @@
-const Loading = ({ message }) => {
-    return <div className='loading-div' >
-        <div>{message}</div>
-        {/* <img src='./images/loading.gif' /> */}
-        <div className="loading-spinner-large"></div>
-    </div >;
-};
-
-export default Loading;
